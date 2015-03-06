59a90f7485d89ca497036811b5cf9611653d6449
Daniel Shiffman <daniel@shiffman.net>
2015-03-06 12:44:06 +0000

    basic recording of keypresses started

--- recorder/recorder.js
+++ recorder/recorder.js
@@ -1,17 +1,68 @@
 var editor;
-var run;
-var dims = [];
+
+var recording = false;
+
+var events = [];
+var json;
+
+function toggleRecording() {
+  recording = !recording;
+  var record = getElement('record');
+  if (recording) {
+    record.html('stop recording');
+    editor.setTheme("ace/theme/cobalt");
+  } else {
+    record.html('start recording');    
+    editor.setTheme("ace/theme/github");
+
+  }
+
+}
+
+function editorChange(e) {
+  if (recording) {
+    var ev = {};
+    ev.time = millis();
+    ev.action = e.data.action;
+    ev.range = e.data.range;
+    ev.text = e.data.text;
+    events.push(ev);
+  
+    json = JSON.stringify(events, null, 2);
+    var output = getElement('recording');
+    output.html(json);
+  }
+
+
+}
+
+function editorSelection(e) {
+  //console.log(e);
+}
+
+function editorCursor(e) {
+  //console.log(e);
+}
+
+
 
 function setup() {
+
   // Basic ACE setup
   editor = ace.edit("editor");
   editor.setTheme("ace/theme/github");
   editor.getSession().setMode("ace/mode/javascript");
   
+  editor.getSession().on('change', editorChange);
+  editor.getSession().selection.on('changeSelection', editorSelection);
+  editor.getSession().selection.on('changeCursor', editorCursor);
+
+  
   // Injecting JavaScript into iFrame with Jquery
   // This is from p5.js web site
   // https://github.com/processing/p5.js-website/blob/master/js/examples.js
   $('#sketchFrame').load(function() {
+    var dims = [];
     var exampleCode = editor.getSession().getValue();
     if (exampleCode.indexOf('new p5()') === -1) {
       exampleCode += '\nnew p5();';
@@ -19,10 +70,10 @@ function setup() {
     if (dims.length < 2) {
       var re = /createCanvas\((.*),(.*)\)/g;
       var arr = exampleCode.split(re);
-     $('#sketchFrame').height(arr[2]+'px');
-   } else {
-     $('#sketchFrame').height(examples.dims[1]+'px');
-   }
+      $('#sketchFrame').height(arr[2]+'px');
+    } else {
+      $('#sketchFrame').height(dims[1]+'px');
+    }
 
     var userScript = $('#sketchFrame')[0].contentWindow.document.createElement('script');
     userScript.type = 'text/javascript';
@@ -32,9 +83,17 @@ function setup() {
   });
 
   noCanvas();
+
+  // Run what is in there
   runIt();
-  run = getElement('run');
+
+  // The run button
+  var run = getElement('run');
   run.mousePressed(runIt);
+
+  // The record button
+  var record = getElement('record');
+  record.mousePressed(toggleRecording);
 }
 
 function runIt() {
